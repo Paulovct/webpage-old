4a70f4c2037c388fd48397877e0065cf69bffed0
paulo <paulovictorpintoalves@gmail.com>
2023-01-11 18:26:07 -0800

    Adicionei imagens e comentarios dos respectivos projetos

--- script.js
+++ script.js
@@ -5,3 +5,21 @@
     const win=window.open('https://github.com/Paulovct/Panda-Calc' ,'_blank')
     win.focus()
    })
+   card[1].addEventListener('click',(evt)=>{
+    const win=window.open('https://github.com/Paulovct/LoginPage---Darktheme' ,'_blank')
+    win.focus()
+   })
+   card[2].addEventListener('click',(evt)=>{
+    const win=window.open('https://github.com/Paulovct/Balls' ,'_blank')
+    win.focus()
+   })
+   card[3].addEventListener('click',(evt)=>{
+    const win=window.open('https://github.com/Paulovct/Card-simples.' ,'_blank')
+    win.focus()
+   })
+   card[4].addEventListener('click',(evt)=>{
+    const win=window.open('https://github.com/Paulovct/paulovct.github.io' ,'_blank')
+    win.focus()
+   })
+
+   //final 5
