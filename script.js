db2cb00390573a874d9f9dd57e9bee4df1a68920
paulo <paulovictorpintoalves@gmail.com>
2023-01-19 21:46:36 -0300

    Att 19/01/2023

--- script.js
+++ script.js
@@ -1,28 +1,49 @@
    const card =[...document.querySelectorAll('.card')]
    const cardt =[...document.querySelectorAll('.card-text')]
-
+   const links=[...document.getElementByClassName('link')]
+   
+   
+   
    
 
+
+
+
+
    card[0].addEventListener('click',(evt)=>{
-    const win=window.open('https://github.com/Paulovct/Panda-Calc' ,'_blank')
+    const win=window.open('https://github.com/Paulovct/Flex-Turismo' ,'_blank')
     win.focus()
    })
    card[1].addEventListener('click',(evt)=>{
-    const win=window.open('https://github.com/Paulovct/LoginPage---Darktheme' ,'_blank')
+    const win=window.open('https://github.com/Paulovct/Save-The-Planet' ,'_blank')
     win.focus()
    })
+
    card[2].addEventListener('click',(evt)=>{
-    const win=window.open('https://github.com/Paulovct/Balls' ,'_blank')
+    const win=window.open('https://github.com/Paulovct/Panda-Calc' ,'_blank')
     win.focus()
    })
    card[3].addEventListener('click',(evt)=>{
-    const win=window.open('https://github.com/Paulovct/Card-simples.' ,'_blank')
+    const win=window.open('https://github.com/Paulovct/LoginPage---Darktheme' ,'_blank')
     win.focus()
    })
    card[4].addEventListener('click',(evt)=>{
+    const win=window.open('https://github.com/Paulovct/Balls' ,'_blank')
+    win.focus()
+   })
+   card[5].addEventListener('click',(evt)=>{
+    const win=window.open('https://github.com/Paulovct/Card-simples.' ,'_blank')
+    win.focus()
+   })
+   card[6].addEventListener('click',(evt)=>{
     const win=window.open('https://github.com/Paulovct/paulovct.github.io' ,'_blank')
     win.focus()
    })
+
+
+
+
+
 
 
    cardt[0].addEventListener('click',(evt)=>{
@@ -30,18 +51,27 @@
     win.focus()
    })
    cardt[1].addEventListener('click',(evt)=>{
-    const win=window.open('https://github.com/Paulovct/LoginPage---Darktheme' ,'_blank')
+    const win=window.open('https://github.com/Paulovct/Panda-Calc' ,'_blank')
     win.focus()
    })
+
    cardt[2].addEventListener('click',(evt)=>{
-    const win=window.open('https://github.com/Paulovct/Balls' ,'_blank')
+    const win=window.open('https://github.com/Paulovct/Panda-Calc' ,'_blank')
     win.focus()
    })
    cardt[3].addEventListener('click',(evt)=>{
-    const win=window.open('https://github.com/Paulovct/Card-simples.' ,'_blank')
+    const win=window.open('https://github.com/Paulovct/LoginPage---Darktheme' ,'_blank')
     win.focus()
    })
    cardt[4].addEventListener('click',(evt)=>{
+    const win=window.open('https://github.com/Paulovct/Balls' ,'_blank')
+    win.focus()
+   })
+   cardt[5].addEventListener('click',(evt)=>{
+    const win=window.open('https://github.com/Paulovct/Card-simples.' ,'_blank')
+    win.focus()
+   })
+   cardt[6].addEventListener('click',(evt)=>{
     const win=window.open('https://github.com/Paulovct/paulovct.github.io' ,'_blank')
     win.focus()
    })
